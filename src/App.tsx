392f623430fbe96c8e19fed2f8c04b824c2ad774
Michael Williams <Michael.Williams@ama.ab.ca>
2020-05-22 10:35:24 -0600

    initial commit: Working example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components'
+import { Theme } from '@amaabca/react-components'
+import { Button, Header } from '@amaabca/react-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <div>
+        <Header>
+          <p>
+            This is in the header
+          </p>
+          <Button>Check out this button</Button>
+        </Header>
+      </div>
+    </ThemeProvider>
   );
 }
 
